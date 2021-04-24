0f733de53c3bbb73a333b4768438dcf4a61dfbc5
tg <2453938187@qq.com>
2021-04-24 21:53:36 +0800

    finish color

---
+++ MaterialDesignDemo/ColorTool.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MaterialDesignDemo
+{
+    /// <summary>
+    /// ColorTool.xaml 的交互逻辑
+    /// </summary>
+    public partial class ColorTool : UserControl
+    {
+        public ColorTool()
+        {
+            DataContext = new ColorToolViewModel();
+            InitializeComponent();
+        }
+    }
+}
